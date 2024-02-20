3968a14d4961b59b291f9f1305ff80b064ff9f0a
ginx836 <141227582+ginx836@users.noreply.github.com>
2024-02-20 23:11:50 +0100

    Update About
    Update About page content and style

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -10,32 +10,44 @@ function page() {
   return (
     <>
       <section className="max-width section-padding flex flex-col tablet:flex-row gap-20 items-center">
-        <div className="relative w-full tablet:w-1/2 h-[calc(300px+20vw)] tablet:h-[600px]">
+        <div className="relative w-full tablet:w-1/2 h-[calc(300px+20vw)] tablet:h-[700px]">
           <Image
-            src={"/images/about.png"}
+            src={"/images/profil-picture.webp"}
             fill
-            alt="about img"
+            alt="Photo de profil"
             className="object-cover"
           />
         </div>
         <div className="tablet:w-1/2">
           <div className="space-y-3 max-tablet:text-center flex flex-col">
-            <p className="px-4 py-2 bg-peach-500 text-grey-600 w-max rounded tablet:w-max max-tablet:mx-auto">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            <p className="px-4 py-2 bg-peach-500 text-grey-600 font-bold italic w-max rounded tablet:w-max max-tablet:mx-auto">
+              "Keep moving forward"
             </p>
-            <h1 className="text-grey-700">Je suis Cédric Bourquin</h1>
-            <p className="text-grey-600">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-              facere unde, autem esse, praesentium tempore exercitationem eos
-              animi quis quaerat repudiandae amet similique, excepturi quam
-              odio. Nisi doloribus in quasi! Et nihil enim mollitia architecto
-              reprehenderit ad nobis facere eius expedita ullam blanditiis
-              nulla, dolore voluptas autem reiciendis animi facilis doloribus
-              maiores illo repellendus dolorem illum excepturi quaerat?
-              Blanditiis, sapiente?
+            <h1 className="text-grey-700 pb-5 max-tablet:text-4xl">
+              Je suis Cédric Bourquin
+            </h1>
+            <p className="text-grey-600 leading-7 pb-5">
+              "Tech Enthusiast", né au début des années 80, j'ai grandi avec
+              l'informatique et le numérique. Parce qu'il n'est jamais trop tard
+              pour apprendre et se lancer j'ai décidé de faire de ma passion mon
+              métier après 21 ans de commerce au service de mes clients.
+              <br />
+              Auto-didacte, grâce à OpenClassrooms j'ai pu mettre un premier
+              pied à l'étrier en développant mes compétences.
+              <br />
+            </p>
+            <p className="font-bold text-lg text-center">
+             Continuer à apprendre, mettre mes compétences au service
+              de vos projets, voilà ce qui m'anime au quotidien.
             </p>
           </div>
         </div>
+      </section>
+      <section className="max-width section-padding">
+        <h2 className="text-grey-700 text-center pb-5 max-tablet:text-xl">
+          "La seule façon de faire du bon travail est d'aimer ce que vous
+          faites" - Steve Jobs
+        </h2>
       </section>
     </>
   );
